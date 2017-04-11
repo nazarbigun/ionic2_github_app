0b765aa793df52885ede02875de0bfeca7397b19
Nazar <nazar.bigun@gmail.com>
2017-04-11 19:31:49 +0300

    hello page changed issues, code, commits views added commits logic added

--- src/providers/commits.ts
+++ src/providers/commits.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { RequestOptions, Http, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class Commits {
 
-url = 'https://api.github.com';
+  url = 'https://api.github.com';
 
-constructor(public http: Http) { }
+  constructor(public http: Http) { }
+
+  createAuthorizationHeader(headers: Headers) {
+    headers.append('Accept', 'application/vnd.github.cloak-preview+json');
+  }
+
+  get(url) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.get(url, {
+      headers: headers
+    });
+  }
 
   load(input): Observable<any[]> {
-    return this.http.get(`${this.url}/search/commits?q=` + input['data'])
+    return this.get(`${this.url}/search/commits?q=` + input['data'])
       .map(res => <any[]>res.json());
   }
 }
